dd5338e7775d99b1ff1ece34aa8f907abee9bbe4
dhangabh <abhishek.dhang.ext@boehringer-ingelheim.com>
2024-05-06 19:38:10 +0530

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const authRouter = require('./routes/auth');
 const listRouter = require('./routes/lists');
 require('dotenv').config();
-const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -14,19 +13,15 @@ app.use(cors());
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
-// app.get('/', (req,res) => {
-//     res.send('Welcome to server');
-// })
+app.get('/', (req,res) => {
+    res.send('Welcome to server');
+})
 
 app.use('/api/auth',authRouter);
 app.use('/api/list',listRouter);
 
 Connection(USERNAME,PASSWORD);
 
-app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
 
 app.listen(process.env.PORT,() => {
     console.log('Server started');
